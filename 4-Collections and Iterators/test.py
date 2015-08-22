f7cc9bdd3652874aae3a49a54bc1b7944d41702b
Gabby <ortmangm@iastate.edu>
2015-08-21 19:34:44 -0500

    add more tests

--- 4-Collections and Iterators/test.py
+++ 4-Collections and Iterators/test.py
@@ -1,20 +1,38 @@
+import unittest
+import collections_and_iterators
+
 """ Collections - TESTS 
     Testing Collections programming examples from collections.py 
 
     Gabby Ortman 
 """
-import unittest 
-import collections 
 
 class TestObjectMethods(unittest.TestCase): 
     def setUp(self): 
-        print collections.__dict__()
-        self.singleLinkList = collections.SinglyLinkedList()
+        self.singleLinkList = collections_and_iterators.SinglyLinkedList()
+        self.doubleLinkList = collections_and_iterators.DoublyLinkedList()
 
     #test that a newly initialized singly linked list has size 0, null head and null cursor
-    def test_empty_list(self): 
+    def test_empty_single_list(self): 
         self.assertEqual(0, self.singleLinkList.size)
+        self.assertIsNone(self.singleLinkList.head)
+        self.assertIsNone(self.singleLinkList.cursor)
 
+    def test_append(self): 
+        pass 
+
+    def test_getitem(self): 
+        pass 
+
+    def test_setitem(self): 
+        pass 
+
+    #test that a newly initated doubly linked list has size 0, null head and null cursor 
+    #this shouldn't be any different from the previous test, BUT, what if inheritance was done incorectly? 
+    def test_empty_double_list(self): 
+        self.assertEqual(0, self.doubleLinkList.size)
+        self.assertIsNone(self.doubleLinkList.head)
+        self.assertIsNone(self.doubleLinkList.cursor)
 
 
 
