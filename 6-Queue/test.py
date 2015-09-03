4cea7e8e1a34d04be0864c25b2a22083f5858bce
Tom <absentisinaventulus@gmail.com>
2015-09-03 09:58:25 -0500

    Added failing tests for Queue.py

--- 6-Queue/test.py
+++ 6-Queue/test.py
@@ -10,17 +10,61 @@ class TestQueue(unittest.TestCase):
 	def setUp(self):
 		pass
 
-	def test_queue(self):
-		pass
+	def test_enqueue(self):
+		q = queue.Queue()
+
+		item1, item2 = 'fish', 'chips'
+
+		q.enqueue(item1)
+		q.enqueue(item2)
+
+		self.assertEquals(item2, q.peek())
+
 
 	def test_dequeue(self):
-		pass
+		q = queue.Queue()
+
+		item1, item2 = 'the tube', 'crumpets'
+
+		q.enqueue(item1)
+		q.enqueue(item2)
+
+		self.assertEquals(item2, q.dequeue())
 
 	def test_size(self):
-		pass
+		q = queue.Queue()
+
+		item1, item2 = 'the queen', 'james bond'
+		self.assertEqual(0, q.size())
+
+		q.enqueue(item1)
+		self.assertEqual(1, q.size())
+
+		q.enqueue(item2)
+		self.assertEqual(2, q.size())
 
 	def test_peek(self):
-		pass
+		q = queue.Queue()
+
+		item1, item2 = 'bangers', 'and mash'
+
+		q.enqueue(item1)
+		q.enqueue(item2)
+
+		self.assertEqual(item2, q.peek())
 
 	def test_clear(self):
-		pass
+		q = queue.Queue()
+
+		item1, item2 = 'rainy weather', 'island nation'
+
+		q.enqueue(item1)
+		q.enqueue(item2)
+
+		q.clear()
+
+		self.assertEqual(0, q.size())
+		self.assertEqual(None, q.peek())
+
+if __name__ == '__main__':
+    unittest.main(verbosity=2)
